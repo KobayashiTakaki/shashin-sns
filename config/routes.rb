0c969597064456d15e3b5f6cafb08b8b0f3b083f
小林鷹生 <takaki@kobayashi-takaki-macbook-pro.local>
2018-11-22 00:39:11 +0900

    implement User signup, login, logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,30 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'sessions/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+
+  get '/login', to: 'sessions#new'
+  get '/signup', to: 'users#new'
+
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
+  resources :users
+
+
 end
+
+# GET / => static_pages#home
+# GET /users/new => User#new
+# POST /users/new => User#create
+#
+# GET /login => Sessions#new
+# POST /login => Sessions#create
+# POST /logout => Sessions#destroy
+#
+# GET /posts/:id => Posts#show
+# GET /posts/new => Posts#new
+# POST /posts => Posts#create
+# POST /posts/:id/comment => Posts#comment
+# POST /posts/:id/like => Posts#like
+#
+#
+# GET /:user_name => Users#show
