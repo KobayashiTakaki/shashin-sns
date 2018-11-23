57e25f94ef8fdf0414fb7c305f053b7cd2479e00
小林鷹生 <takaki@kobayashi-takaki-macbook-pro.local>
2018-11-23 20:27:24 +0900

    implement friendly login redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:session][:name])
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to root_url
+      redirect_back_or root_url
     else
       flash.now[:danger] = "ログインに失敗しました。"
       render 'new'
