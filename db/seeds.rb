7902e061f4814d8989217ea6f408bbdc3fed0586
小林鷹生 <takaki@kobayashi-takaki-macbook-pro.local>
2018-11-28 16:38:53 +0900

    seeds and fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "star",
+            display_name: "Star Butterfly",
+            email: "star@example.com",
+            password: "password",
+            password_confirmation: "password")
+
+10.times do |n|
+  display_name = Faker::FamilyGuy.unique.character
+  name = display_name.split(' ')[0].downcase
+  email = "#{name}@example.com"
+  password = "password"
+  User.create!(name: name,
+              display_name: display_name,
+              email: email,
+              password: password,
+              password_confirmation: password)
+end
