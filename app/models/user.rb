d89baf12f68a4c09631300748182f33631e8fa5a
小林鷹生 <takaki@kobayashi-takaki-macbook-pro.local>
2018-12-04 23:41:06 +0900

    initialize devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   mount_uploader :picture, PictureUploader
   before_save :downcase
   has_secure_password
