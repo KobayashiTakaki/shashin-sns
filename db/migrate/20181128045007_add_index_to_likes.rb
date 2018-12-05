31d5e6608f617193bc3d35579ad41abc7d15a111
小林鷹生 <takaki@kobayashi-takaki-macbook-pro.local>
2018-12-05 23:59:37 +0900

    fixed typo

--- db/migrate/20181128045007_add_index_to_likes.rb
+++ db/migrate/20181128045007_add_index_to_likes.rb
@@ -1,5 +1,5 @@
 class AddIndexToLikes < ActiveRecord::Migration[5.2]
   def change
-    add_index :Likes, [:user_id, :post_id], unique: true
+    add_index :likes, [:user_id, :post_id], unique: true
   end
 end
