2776b4f201892a2bc883e15d3ed108b706e94000
kobayashitakaki <takaki@takakisan.com>
2018-12-06 22:51:07 +0900

    implement pagination

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
     if current_user.following.empty?
-      @feed_items = Article.order("created_at DESC").page(1).per(20)
+      @feed_items = Article.order("created_at DESC").page(params[:page]).per(20)
     else
-      @feed_items = current_user.feed.order("created_at DESC").page(1).per(20)
+      @feed_items = current_user.feed.order("created_at DESC").page(params[:page]).per(20)
     end
   end
 end
