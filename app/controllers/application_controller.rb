4f4a7edead1ebb7825d15a70b6a46cab10d49b17
kobayashitakaki <takaki@takakisan.com>
2018-12-07 04:02:53 +0900

    modified check user logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     def logged_in_user
       unless user_signed_in?
         store_location
-        flash[:danger] = "ログインしてください"
+        sign_out current_user
         redirect_to root_url
       end
     end
