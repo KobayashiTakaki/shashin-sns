72aa9a0311e2ae267ce1d1c9221327e716843ffb
kobayashitakaki <takaki@takakisan.com>
2018-12-06 21:49:01 +0900

    implement feed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-    @feed_items = Article.all
+    if current_user.following.empty?
+      @feed_items = Article.order("created_at DESC").page(1).per(20)
+    else
+      @feed_items = current_user.feed.order("created_at DESC").page(1).per(20)
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -64,7 +64,10 @@ class User < ApplicationRecord
   end
 
   def feed
-
+    following_ids = "SELECT followee_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Article.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
   # ユーザーをフォローする
