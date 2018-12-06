fcb3ef22de3c4927cdc36dfdbcd1d1d7efa299e6
kobayashitakaki <takaki@takakisan.com>
2018-12-07 04:13:49 +0900

    remove before_action from static_pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,4 @@
 class StaticPagesController < ApplicationController
-  before_action :logged_in_user
-  
   def home
     if current_user.following.empty?
       @feed_items = Article.order("created_at DESC").page(params[:page]).per(20)
