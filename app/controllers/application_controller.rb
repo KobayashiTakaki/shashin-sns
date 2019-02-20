7cdcbbd46834ce60e9ac8951ef5482156a67c491
kobayashitakaki <o.share.thief@gmail.com>
2019-02-20 20:11:48 +0900

    disable facebook login and implement email/password login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include SessionsHelper
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :display_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :display_name])
+  end
 
   private
   # ログイン済みユーザーかどうか確認
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[facebook]
+         :rememberable, :validatable
   mount_uploader :picture, PictureUploader
   before_save :downcase
   #has_secure_password
